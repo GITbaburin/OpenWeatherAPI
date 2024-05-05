95126a3bee310f191eb3f11baf62b56925efbdba
Baburin Robert <88195319+GITbaburin@users.noreply.github.com>
2024-05-05 20:49:38 +0800

    Update config.py

--- weather/config.py
+++ weather/config.py
@@ -1,5 +1,5 @@
 USE_ROUNDED_COORDS = True
-OPENWEATHER_API = "0254bcf2c9ada8933ad29c7a401f30b1" # You can create an API key at openweathermap.org, it is easy and Free
+OPENWEATHER_API = <YOUR_OPENWEATHER_API_KEY> # You can create an API key at openweathermap.org, it is easy and Free
 OPENWEATHER_URL = (
     "https://api.openweathermap.org/data/2.5/weather?"
     "lat={latitude}&lon={longitude}&"
